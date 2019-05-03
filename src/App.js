e9e793e8f7b7b07b92eca031a2d33c7bc70b6c8f
Bonifase <orwabonifase@gmail.com>
2019-05-03 16:21:39 +0300

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Form from "./components/Form";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = "4678a2d7b2b2fca31d3eb917358ad0d8";
+
+export default class App extends Component {
+  state = {
+    recipes: []
+  };
+  getRecipe = async e => {
+    e.preventDefault();
+    const recipeName = e.target.elements.recipeName.value;
+    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&page=2`);
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">Search Recipe</header>
+        <Form getRecipe={this.getRecipe} />
+        {this.state.recipes.map(data => {
+          return (
+            <div key={data.recipe_id}>
+              <img src={data.image_url} alt={data.title} />
+              <p>{data.title}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Form = props => (
+  <div>
+    <form onSubmit={props.getRecipe}>
+      <input type="text" name="recipeName" />
+      <button>Search</button>
+    </form>
+  </div>
+);
+export default Form;
